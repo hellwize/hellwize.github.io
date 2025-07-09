2ccfd79837db242945b59252a983da226a759e03
Evan Wahyu Ezra Manurung <vanpendidikan09@gmail.com>
2025-07-09 20:02:27 +0700

    p

--- service.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', function(e) {
-  console.log('Service Worker: Terpasang');
-});
-
-self.addEventListener('fetch', function(e) {
-  console.log('Service Worker: Mengambil', e.request.url);
-});
